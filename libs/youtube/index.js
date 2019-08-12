baa7eae922e3669e332a4a522d35d56c72e8b07b
Ganesh Jaiwal <ganesh.jaiwal@vshsolutions.com>
2019-08-12 18:31:59 +0530

    updated fevicon and added youtube module

---
+++ libs/youtube/index.js
@@ -0,0 +1,7 @@
+export * from "./src/youtube.module";
+export * from "./src/components";
+export * from "./src/services";
+export * from "./src/models";
+export * from "./src/+state";
+
+// https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.search.list?part=id%252Csnippet&q=test&_h=6&
